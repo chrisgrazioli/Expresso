a07f7e482e7c2e136c20cc8be95043973a4de1f9
chrisgrazioli <chrisgrazioli@gmail.com>
2018-02-10 00:54:27 -0500

    worked and tested up to employeesRouter.put()

--- api/employees.js
+++ api/employees.js
@@ -4,6 +4,19 @@ const employeesRouter = express.Router();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+employeesRouter.param('/:employeeId', (req, res, next, employeeId)=>{
+  db.get('SELECT * FROM Employee WHERE id = $id',{$id:employeeId}, (error, employee)=>{
+    if(error){
+      next(error);
+    }else if(!employee){
+      res.sendStatus(404);
+    }else{
+      req.employee = employee;
+      next();
+    }
+  });
+});
+
 employeesRouter.get('/', (req, res)=>{
   db.all('SELECT * FROM Employee WHERE is_current_employee = 1', (error, employees)=>{
     if(error){
@@ -41,14 +54,55 @@ employeesRouter.post('/', (req, res)=>{
   }
 });
 
-employeesRouter.get('/:employeeId', (req, res)=>{
+employeesRouter.get('/:employeeId', (req, res, next)=>{
   db.get('SELECT * FROM Employee WHERE id = $id',{$id:req.params.employeeId}, (error, employee)=>{
     if(error){
+      next(error);
+    }else if(!employee){
       res.sendStatus(404);
     }else{
       res.status(200).json({employee: employee});
     }
   });
 });
+
+const validateEmployee = (req, res, next)=>{
+  console.log(req.body.employee);
+  if(!req.body.employee.name ||
+    !req.body.employee.position ||
+    !req.body.employee.wage){
+    return res.sendStatus(400);
+  }else{
+    next();
+  }
+};
+
+employeesRouter.put('/:employeeId', validateEmployee, (req, res, next)=>{
+  const name= req.body.employee.name,
+        position= req.body.employee.position,
+        wage= req.body.employee.wage,
+        isCurrentEmployee= req.body.employee.isCurrentEmployee;
+  const sql='UPDATE Employee SET name = $name, position = $position, wage = $wage, is_current_employee = $isCurrentEmployee WHERE Employee.id = $id';
+  const values ={
+    $name: name,
+    $position: position,
+    $wage : wage,
+    $isCurrentEmployee : 1,
+    $id : req.params.employeeId
+  };
+  db.run(sql, values, function(error){
+    if(error){
+      next(error);
+    }else{
+      db.get('SELECT * FROM Employee WHERE Employee.id = $id',{$id : req.params.employeeId}, (error, employee)=>{
+        res.status(200).json({employee : employee});
+      });
+    }
+  });
+});
+
+//employeesRouter.delete('/:employeeId', (req, res, next)=>{
+  //req.employee.id //from employeesRouter.param
+//});
 
 module.exports = employeesRouter;
